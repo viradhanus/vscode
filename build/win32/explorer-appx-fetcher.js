0029bea9be45be615a2b6bda8af9c10f67198473
deepak1556 <hop2deep@gmail.com>
2022-05-31 22:06:51 +0900

    chore: bundle and sign explorer appx

---
+++ build/win32/explorer-appx-fetcher.js
@@ -0,0 +1,53 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.downloadExplorerAppx = void 0;
+const debug = require("debug");
+const fs = require("fs-extra");
+const path = require("path");
+const product = require("../../product.json");
+const get_1 = require("@electron/get");
+const d = debug('explorer-appx-fetcher');
+async function downloadExplorerAppx(outDir, quality = 'stable', targetArch = 'x64') {
+    const fileNamePrefix = quality === 'insider' ? 'code_insiders' : 'code';
+    const fileName = `${fileNamePrefix}_explorer_${targetArch}.appx`;
+    if (await fs.pathExists(path.resolve(outDir, 'fileName'))) {
+        return;
+    }
+    if (!await fs.pathExists(outDir)) {
+        await fs.mkdirp(outDir);
+    }
+    d(`downloading ${fileName}`);
+    const artifact = await (0, get_1.downloadArtifact)({
+        isGeneric: true,
+        version: '1.0.0',
+        artifactName: fileName,
+        mirrorOptions: {
+            mirror: 'https://github.com/microsoft/vscode-explorer-command/releases/download/',
+            customDir: '1.0.0',
+            customFilename: fileName
+        }
+    });
+    await fs.copy(artifact, path.join(outDir, fileName));
+}
+exports.downloadExplorerAppx = downloadExplorerAppx;
+async function main() {
+    const outputDir = process.env['VSCODE_EXPLORER_APPX_DIR'];
+    let arch = process.env['VSCODE_ARCH'];
+    if (!outputDir) {
+        throw new Error('Required build env not set');
+    }
+    if (arch === 'ia32') {
+        arch = 'x86';
+    }
+    await downloadExplorerAppx(outputDir, product.quality, arch);
+}
+if (require.main === module) {
+    main().catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+}
---
+++ build/win32/explorer-appx-fetcher.ts
@@ -0,0 +1,63 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+import * as debug from 'debug';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import * as product from '../../product.json';
+import { downloadArtifact } from '@electron/get';
+
+const d = debug('explorer-appx-fetcher');
+
+export async function downloadExplorerAppx(outDir: string, quality: string = 'stable', targetArch: string = 'x64'): Promise<void> {
+	const fileNamePrefix = quality === 'insider' ? 'code_insiders' : 'code';
+	const fileName = `${fileNamePrefix}_explorer_${targetArch}.appx`;
+
+	if (await fs.pathExists(path.resolve(outDir, 'fileName'))) {
+		return;
+	}
+
+	if (!await fs.pathExists(outDir)) {
+		await fs.mkdirp(outDir);
+	}
+
+	d(`downloading ${fileName}`);
+	const artifact = await downloadArtifact({
+		isGeneric: true,
+		version: '1.0.0',
+		artifactName: fileName,
+		mirrorOptions: {
+			mirror: 'https://github.com/microsoft/vscode-explorer-command/releases/download/',
+			customDir: '1.0.0',
+			customFilename: fileName
+		}
+	});
+
+	await fs.copy(artifact, path.join(outDir, fileName));
+}
+
+async function main(): Promise<void> {
+	const outputDir = process.env['VSCODE_EXPLORER_APPX_DIR'];
+	let arch = process.env['VSCODE_ARCH'];
+
+	if (!outputDir) {
+		throw new Error('Required build env not set');
+	}
+
+	if (arch === 'ia32') {
+		arch = 'x86';
+	}
+
+	await downloadExplorerAppx(outputDir, (product as any).quality, arch);
+}
+
+if (require.main === module) {
+	main().catch(err => {
+		console.error(err);
+		process.exit(1);
+	});
+}
